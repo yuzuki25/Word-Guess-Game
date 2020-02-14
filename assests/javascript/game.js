8ee1179300f2f44dfab1de45aa9703871e51cf11
zucchini <yuzuki.needham@gmail.com>
2020-02-14 16:59:56 -0700

    Roughly configured js file. Added words(name) variable that should display words as ---

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,17 +1,51 @@
 var userguess = ['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 var wins = 0;
 var losses = 0;
+var word = [
+    "cristiano ronaldo",
+    "lionel messi",
+    "zlatan ibrahimovic",
+    "keisuke honda",
+    "carles puyol",
+    "andres iniesta",
+    "clint dempsey",
+    "manuel neuer",
+    "franck ribery",
+    "robert lewandowski",
+    "arjen robben",
+    "neven subotic",
+    "giorgio chillini",
+    "vincent kompany",
+    "eden hazard"];
+
+var splitWord = word.split("");
+
+console.log(word)
+console.log(splitWord)
 
 //First problem is making the 'press any key to start element'
 document.onkeydown = function() {
     var start = "any key"
 }
 
+
 //After pressing any button, it should allow you to start guessing
 document.onkeyup = function() {
     var userguess = String.fromCharCode(event.keyCode).
-        toUpperCase();
+        toLowerCase();
 
         console.log(userguess);
+
+        var word = words[Math.floor(Math.random() * words.length)];
+
+        var answerArray = [];
+        for (var i = 0; i < word.length; i++); {
+            answerArray[i] = "_";
+        }
+
+        var remainingLetters = word.length;
+        for (var r = 7; r === 0; r--); {
+            if r === 0, endGame
+        }
 
 }
