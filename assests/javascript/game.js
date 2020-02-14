a47a0ad3630ef8b81d5f558d81b6243fdf0185a4
zucchini <yuzuki.needham@gmail.com>
2020-02-14 13:00:12 -0700

    Creating a skeleton for the html and starting JS w/ my initial thoughts

---
+++ assests/javascript/game.js
@@ -0,0 +1,17 @@
+var options = [a, b, c];
+var wins = 0;
+var losses = 0;
+
+//First problem is making the 'press any key to start element'
+document.onkeyup = function() {
+    var start = 
+}
+
+//After pressing any button, it should allow you to start guessing
+document.onkeyup = function() {
+    var userguesss = string.fromCharCode(event.keyCode).
+        toUpperCase();
+
+        console.log(userguess);
+
+}
