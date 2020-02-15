034b1178af636c040b4c32f79d33605227aa7d84
zucchini <yuzuki.needham@gmail.com>
2020-02-14 18:51:15 -0700

    Read into making a hangman game and mixed what I was thinking into it. Not sure how to link the js and html

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,7 +1,7 @@
 var userguess = ['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 var wins = 0;
 var losses = 0;
-var word = [
+var words = [
     "cristiano ronaldo",
     "lionel messi",
     "zlatan ibrahimovic",
@@ -18,10 +18,10 @@ var word = [
     "vincent kompany",
     "eden hazard"];
 
-var splitWord = word.split("");
+// var splitWord = word.split("");
 
-console.log(word)
-console.log(splitWord)
+// console.log(word)
+// console.log(splitWord)
 
 //First problem is making the 'press any key to start element'
 document.onkeydown = function() {
@@ -44,8 +44,19 @@ document.onkeyup = function() {
         }
 
         var remainingLetters = word.length;
-        for (var r = 7; r === 0; r--); {
-            if r === 0, endGame
+        // for (var r = 7; r === 0; r--); {
+        //     if r === 0, endGame
+        // }
+
+        while (remainingLetters > 0) {
+            document.write(answerArray.join(" "));
+
+                for (var j = 0; j < word.length; j++) {
+                    if (word[j] === userguess) {
+                        answerArray[j] = userguess;
+                        remainingLetters--;
+                    }
+                }
         }
 
 }
