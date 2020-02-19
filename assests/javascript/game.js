cd85323be6301084f4098ae1de4ce0e76a8880df
zucchini <yuzuki.needham@gmail.com>
2020-02-18 23:03:00 -0700

    Added pseudocode for parts that I could not finish

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -45,12 +45,22 @@ document.querySelector("#answerArrays").innerHTML = blankSpaces;
 document.querySelector("#winCount").innerHTML = win;
 document.querySelector("#lossCount").innerHTML = loss;
 
+
+//press any button to start
+// if (onkeydown[]) {
+//     start the game/run code?
+// }
+
+
+//This was made in theory to understand what key was pressed by the user out of the userguess variable
 var myGuess;
 document.onkeydown = function(event) {
     myGuess = event.key;
     myGuess = onkeydown(userguess);
 }
 
+
+//This was in theory supposed to work to fill in the underscores with the user's guesses
 for (var j = 0; j < word.length; j++) {
     if (word[j] === myGuess) {
         answerArray[j] = myGuess;
@@ -58,33 +68,42 @@ for (var j = 0; j < word.length; j++) {
     }
 }
 
+
+//This is made to display the number of wrong guesses the player has remaining
 var wrong = 8;
 document.querySelector("#guessRemaining").innerHTML = wrong;
-//Game loop from here.
+
+
+
+//Game loop from here. In the game loop, I was planning on making it so that the user can keep guessing until they get the answer or run out of wrong guesses allowed
 while (remainingLetters > 0) {
     if (myGuess === answerArray[j]) {
         answerArray.join("#answerArrays") = myGuess;
-
+        //This is to make the letter that was guessed show up if it was a right guess
     }
 
     if (myGuess != answerArray[j]) {
         wrong--;
         document.querySelector("#guessed").innerHTML = myGuess;
+        //This is to make the letter that was guessed show up in the wrong guesses and count down the number of remaining guesses
     }
 
     else (wrong === 0) 
         losses++;
-    
+        //When you run out of guesses to make, this should update the count of losses
 
 }
 
-if (reaminingLetters === 0) {
+if (remainingLetters === 0) {
     win++;
 }
+//This should update the win count when the word is fully guessed
 
 
-
-
+//I wasn't sure how to reset the game but maybe it would have turned out like this
+// if (remainingLetters === 0 {
+//     reset game or loop back to beginning with the random word calculation maybe?
+// })
 
 
 
